91e219e6d9ed5b5e1fcba7199f57a3e2095af163
thanhnguyen198 <thanh.nguyen1@tomosia.com>
2022-10-20 10:36:39 +0700

    Update twitter library, up version FBSDK

--- tms-social-lib/Classes/Twitter/TMSTwitter.swift
+++ tms-social-lib/Classes/Twitter/TMSTwitter.swift
@@ -2,20 +2,22 @@
 //  tms-social-lib
 
 import Foundation
-import TwitterCore
-import TwitterKit
+import OAuthSwift
 
 public struct TMSConfiguration {
     public let consumerKey: String
     public let consumerSecret: String
     public var accessGroup: String?
+    public let requestTokenURL: String
+    public let authorizeURL: String
+    public let accessTokenURL: String
 }
 
-public struct TMSTwitterSession {
-    public var authToken: String
-    public var authTokenSecret: String
-    public var userName: String
-    public var userID: String
+public struct TMSTwitterSession: Codable {
+    let userId: String
+    let screenName: String
+    let oauthTokenSecret: String
+    let oauthToken: String
 }
 
 public enum TMSTwitterResult {
@@ -24,16 +26,14 @@ public enum TMSTwitterResult {
 }
 
 public class TMSTwitter {
+    private var oauthSwift: OAuth1Swift
     /// Initialize TMSTwitter
-    public init() {
-    }
-
-    /// Start Twitter with a consumer key, secret, and keychain access group in TMSConfiguration
-    /// - Parameter configuration: Contains consumerKey, consumerSecret and accessGroup on Twitter App Page
-    class func configuration(_ configuration: TMSConfiguration) {
-        TWTRTwitter.sharedInstance().start(withConsumerKey: configuration.consumerKey,
-                                           consumerSecret: configuration.consumerSecret,
-                                           accessGroup: configuration.accessGroup)
+    public init(configuration: TMSConfiguration) {
+        oauthSwift = OAuth1Swift(consumerKey: configuration.consumerKey,
+                                 consumerSecret: configuration.consumerSecret,
+                                 requestTokenUrl: configuration.requestTokenURL,
+                                 authorizeUrl: configuration.authorizeURL,
+                                 accessTokenUrl: configuration.accessTokenURL)
     }
 
     /// Triggers user authentication with Twitter.
@@ -43,66 +43,26 @@ public class TMSTwitter {
     ///     - completion: The completion block will be called after authentication is successful or if there is an error.
     /// - Warning:
     ///     - This method requires that you have set up your `consumerKey` and `consumerSecret` in TMSConfiguration.
-    public func login(completion: @escaping (TMSTwitterResult) -> Void) {
-        TWTRTwitter.sharedInstance().logIn { session, error in
-            if let session = session?.mapTMSSession() {
-                completion(.success(session))
-            } else if let error = error {
+    public func login(callBackURL: String, completion: @escaping (TMSTwitterResult) -> Void) {
+        guard let callBackURL = URL(string: callBackURL) else { return }
+        oauthSwift.authorize(withCallbackURL: callBackURL, completionHandler: { response in
+            switch response {
+            case let .success(result):
+                let json = try? JSONSerialization.data(withJSONObject: result.parameters)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                guard let json = json else { return }
+                let twitterSeession = try? decoder.decode(TMSTwitterSession.self, from: json)
+                completion(.success(twitterSeession!))
+            case let .failure(error):
                 completion(.failure(error))
             }
-        }
+        })
     }
-
-    /// Triggers user authentication with Twitter. Allows the developer to specify the presenting view controller.
-    /// This method will present UI to allow the user to log in if there are no saved Twitter login credentials.
-    /// - Parameters:
-    ///   - controller: The view controller that will be used to present the authentication view.
-    ///   - completion: The completion block will be called after authentication is successful or if there is an error.
-    /// - Warning:
-    ///     - This method requires that you have set up your `consumerKey` and `consumerSecret` in TMSConfiguration.
-    public func login(with controller: UIViewController, completion: @escaping (TMSTwitterResult) -> Void) {
-        TWTRTwitter.sharedInstance().logIn(with: controller) { session, error in
-            if let session = session?.mapTMSSession() {
-                completion(.success(session))
-            } else if let error = error {
-                completion(.failure(error))
-            }
-        }
-    }
-
-    /// Finish the `SFSafariViewController` authentication loop. This method should
-    /// be called from application:openURL:options inside the application delegate.
-    ///
-    /// This method will verify an authentication token sent by the Twitter API to
-    /// finish the web-based authentication flow.
-    /// - Parameters:
-    ///   - application: The `UIApplication` instance received as a parameter
-    ///   - url: The `URL` instance received as a parameter
-    ///   - options: The options dictionary received as a parameter.
-    /// - Returns: Boolean specifying whether this URL was handled by Twitter Kit or not
-    public class func application(_ application: UIApplication, open url: URL, options: [AnyHashable: Any] = [:]) -> Bool {
-        return TWTRTwitter.sharedInstance().application(application, open: url, options: options)
-    }
-
-    /// Finish the `SFSafariViewController` authentication loop. This method should
-    /// be called from scene:openURLContexts inside the scene delegate.
-    ///
-    /// This method will verify an authentication token sent by the Twitter API to
-    /// finish the web-based authentication flow.
-    /// - Parameters:
-    ///   - scene: The scene that UIKit asks to open the URL.
-    ///   - urlContexts: One or more UIOpenURLContext objects. Each object contains one URL to open and any additional information needed to open that URL.
-    @available(iOS 13.0, *)
-    public class func scene(_ scene: UIScene, openURLContexts urlContexts: Set<UIOpenURLContext>) {
-        TWTRTwitter.sharedInstance().scene(scene, openURLContexts: urlContexts)
+    
+    /// Handle callback url which contains now token information
+    public class func handleURL(url: URL) {
+        OAuthSwift.handle(url: url)
     }
 }
 
-extension TWTRSession {
-    public func mapTMSSession() -> TMSTwitterSession {
-        return TMSTwitterSession(authToken: authToken,
-                                 authTokenSecret: authTokenSecret,
-                                 userName: userName,
-                                 userID: userID)
-    }
-}
